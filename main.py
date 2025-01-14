91189e6ebb7d7e8511e9df6ad900ea74c5146df9
maxprograms <maxpreble@gmail.com>
2025-01-14 14:39:47 -0500

    Added screen drawing, player sprite, and movement functionality

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        screen.fill('black')
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updateable:
+            obj.update(dt)
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
